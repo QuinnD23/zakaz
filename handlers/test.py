72748428b7caa94b09c0b4f0d078ccd357ddbdd7
QuinnD23 <danilquinn21@gmail.com>
2021-11-26 17:45:35 +0300

    1

--- handlers/test.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-now_named_day = str(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=3))).strftime("%A"))
-
-text = "Thursday"
-if text == now_named_day:
-    print(now_named_day)
